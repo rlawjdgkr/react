8524740f7530e399c2c7076f3b1976b7f3daae0d
rlawjdgkr <irlawjdgkr@naver.com>
2025-02-09 19:20:02 +0900

    modal open close

--- src/components/Users/AddUsers.jsx
+++ src/components/Users/AddUsers.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styles from './AddUsers.module.css';
 import Card from '../ui/Card';
 import Button from '../ui/Button';
+import ErrorModal from '../ui/Modal/ErrorModal';
 
 const AddUsers = ({onAddUser}) => {
   
@@ -11,6 +12,11 @@ const AddUsers = ({onAddUser}) => {
     username: '',
     age: ''
   });
+
+  // 에러가 났을 때 에러의 데이터를 관리할 상태변수
+  // error -> {title : '에러제목', message: '에러의 내용' }  
+  const [error, setError] = useState(null);
+
   const handleName = e=> {
     setUserValue({
       ...userValue,
@@ -29,6 +35,22 @@ const AddUsers = ({onAddUser}) => {
 
     const handleSubmit = e => {
       e.preventDefault();
+      // 입력값 검증
+      if(!userValue.username.trim()){
+        setError({
+          title:'유효하지 않는 입력값'
+          ,message:'입력값은 필수입니다.'
+          
+        });
+        return;
+      }
+      if(+userValue.age < 1){
+        setError({
+          title:'유효하지 않는 나이 범위'
+          ,message:'나이는 0이나 음수가 될 수 없습니다.'
+        });
+        return;
+      }
       onAddUser({
         ...userValue
         ,id: Math.random().toString()
@@ -38,7 +60,13 @@ const AddUsers = ({onAddUser}) => {
         age:''
       })
     }
+    // 에러모달을 닫아주는 함수
+    const closeModal = ()=>{
+        setError(null);
+    }
   return (
+    <>
+   {error && <ErrorModal title={error.title} message ={error.message} onClose={closeModal}/>}
     <Card className={styles.input}>
       <form onSubmit={handleSubmit}>
         <label htmlFor="username">이름</label>
@@ -58,6 +86,7 @@ const AddUsers = ({onAddUser}) => {
         <Button type="submit">가입하기</Button>
       </form>
     </Card>
+    </>
   );
 };
 
