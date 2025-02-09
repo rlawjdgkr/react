2e146b101c0c77affb5dd2fbfff7c9696e4284ff
rlawjdgkr <irlawjdgkr@naver.com>
2025-02-09 18:46:06 +0900

    유저정보 및 관리 앱 탬플릿릿

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 
+import { useState } from 'react';
 import './App.css';
+import AddUsers from './components/Users/AddUsers';
+import UserList from './components/Users/UserList'
+
+
 
 
 // 컴포넌트
 const App = () => {
+
+  // 회원들이 저장될 배열
+  const [userList, setUserList] = useState([]);
+
+  // 입력한 회원정보를 가져오는 함수
+  const addUserInfo = (user) => {
+    setUserList([...userList , user])
+
+  }
   return <>
+  <AddUsers onAddUser ={addUserInfo}/>
+  <UserList users={userList}/>
+  
   </>;
 };
 
